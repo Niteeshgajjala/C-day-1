06ed8cefb47b0754901140c95c0fcc18801d32bc
Niteeshgajjala <114060882+Niteeshgajjala@users.noreply.github.com>
2025-04-03 11:57:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,12 @@
 ﻿using System;
 
-namespace Avg
+namespace time
 {
     class Program
     {
         static void Main()
         {
-            double a, b, c;
-            Console.WriteLine("Enter The First Number: ");
-            a = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter The Second Number: ");
-            b = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter The Third Number: ");
-            c = double.Parse(Console.ReadLine());
-
-            double avg = (a + b + c) / 3;
-            Console.WriteLine("Average of three numbers is: " + avg);
+            Console.WriteLine($"Time Zone: { DateTime.Now}");
         }
     }
 }
