67c00c97745c9f71b9865e1b8cce805a8328cb3d
Niteeshgajjala <114060882+Niteeshgajjala@users.noreply.github.com>
2025-04-03 11:56:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Avg
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a, b, c;
+            Console.WriteLine("Enter The First Number: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The Second Number: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The Third Number: ");
+            c = double.Parse(Console.ReadLine());
+
+            double avg = (a + b + c) / 3;
+            Console.WriteLine("Average of three numbers is: " + avg);
+        }
+    }
+}
